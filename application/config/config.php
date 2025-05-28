2da3da85a24954a4033a4d408f41bcce090618fa
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2025-05-28 08:18:43 +0700

    feat: set config with env

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = getenv('BASE_URL');
 if (!$config['base_url'] && !is_cli()) {
 	$config['base_url'] = (is_https()) ? "https" : "http";
 	$config['base_url'] .= "://".($_SERVER['HTTP_X_FORWARDED_HOST'] ?? $_SERVER['HTTP_HOST']);
@@ -331,7 +331,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/userguide3/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = getenv('CI_ENCRYPTION_KEY');
 
 /*
 |--------------------------------------------------------------------------
