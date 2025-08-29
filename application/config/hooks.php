f1bcfae628d6f6e92a5360a1e313c119f41aa290
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2025-08-29 23:17:01 +0700

    feat: laravel-like routes

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/userguide3/general/hooks.html
 |
 */
+
+if (!is_cli()) {
+	$hook = Luthier\Hook::getHooks();
+}
